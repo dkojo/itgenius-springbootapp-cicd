d0379f743e983fa1f6821a86dc1c3f5a16319e1d
dkojo <danielkojomegbenu@yahoo.com>
2024-11-27 06:18:22 -0500

    updated codebase

--- terraform/ec2-instances/main.tf
+++ terraform/ec2-instances/main.tf
@@ -217,7 +217,7 @@ resource "aws_security_group" "monitoring_sg" {
 # Define instances
 resource "aws_instance" "nexus_server" {
   ami                    = var.instance_ami
-  instance_type          = var.instance_type
+  instance_type          = t2.medium
   key_name               = var.instance_key_name
   vpc_security_group_ids = [aws_security_group.nexus_sg.id]
   subnet_id              = var.instance_subnet_id
